14ab9d6ca122b3bddfcdb3206cefb3bb02787d30
Mariano Carballal <mariano.carballal@zalando.de>
2019-03-01 15:15:08 +0100

    Add support to ES6 modules for the default agent

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = require('./lib/agent');
+module.exports.default = require('./lib/agent');
 module.exports.HttpsAgent = require('./lib/https_agent');
 module.exports.constants = require('./lib/constants');
