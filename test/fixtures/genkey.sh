92b8482c84dce6133cf75fe8b539d60ec12f280d
Radoslav Kirilov <radoslav.kirilov@corva.ai>
2024-12-28 15:48:21 +0200

    chore: fix tests generate bigger key
    Fixes `Error: error:0A00018F:SSL routines::ee key too small`

--- test/fixtures/genkey.sh
+++ test/fixtures/genkey.sh
@@ -6,11 +6,11 @@ openssl req -new -x509 -nodes -days 9999 -config ca.cnf -keyout ca.key -out ca.p
 
 # Generate server key
 # openssl genrsa -out server.key 2048
-openssl genrsa -out agenttest-key.pem 1024 
+openssl genrsa -out agenttest-key.pem 2048
 
 # Generate a certificate signing request for server.key
 # openssl req -new  -key server.key -out server.csr
-openssl req -new -key agenttest-key.pem -out agenttest.csr -config server.cnf 
+openssl req -new -key agenttest-key.pem -out agenttest.csr -config server.cnf
 
 # Sign the csr with the ca certificate, generating server.pem
 openssl x509 -req -extfile server.cnf -days 999 -passin "pass:password" -extensions v3_req -in agenttest.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out agenttest-cert.pem
